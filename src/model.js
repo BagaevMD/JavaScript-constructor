b60f17a26e9fb106aeca68bf2f158cb721ec673b
BagaevMD <maksim.bagaev@data3.ru>
2020-09-15 13:19:50 +0500

    refactoring and adding  sidebar

--- src/model.js
+++ src/model.js
@@ -5,25 +5,49 @@ import {
     TextColumnsBlock,
     TextBlock
 } from './classes/blocks'
+import {css} from './utils';
 
 export const model = [
     new TitleBlock('Конструктор сайтов на чистом JavaScript!', {
         tag: 'h2',
-        styles: 'background: linear-gradient(to right, #ff0099, #493240);color: #fff;padding: 1.5rem; text-align: center;'
+        styles: css({
+            background: 'linear-gradient(to right, #ff0099, #493240)',
+            color: '#fff',
+            padding: '1.5rem',
+            'text-align': 'center',
+        }),
     }),
     new ImageBlock(image, {
-        styles: 'padding: 2rem 0; display: flex; justify-content: center;',
+        styles: css({
+            padding: '2rem 0',
+            display: 'flex',
+            'justify-content': 'center',
+        }),
         alt: 'my image',
-        imageStyles: 'width: 500px; height: auto;'
+        imageStyles: css({
+            width: '500px',
+            height: 'auto',
+        })
     }),
     new TextColumnsBlock([
         'Приложение на чистом JavaScript, без использования библиотек',
         'Внедрили немного SOLID и ООП',
         'Круто, модно, молодежно. Спасибо за внимание'
     ], {
-        styles: 'padding: 2rem 0; color: #fff; background: linear-gradient(to bottom, #8e2de2, #4a00e0); font-weight: bold; text-align: center;'
+        styles: css({
+            padding: '2rem 0',
+            color: '#fff',
+            background: 'linear-gradient(to bottom, #8e2de2, #4a00e0)',
+            'font-weight': 'bold',
+            'text-align': 'center',
+        })
     }),
     new TextBlock('Тут могла быть ваша реклама', {
-        styles: 'background: linear-gradient(to left, #f2994a, #f2c94c); font-weight: bold; padding: 1rem; text-align: center;'
+        styles: css({
+            background: 'linear-gradient(to left, #f2994a, #f2c94c)',
+            'font-weight': 'bold',
+            padding: '1rem',
+            'text-align': 'center',
+        })
     })
 ];
